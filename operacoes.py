f80aa31462790331868590d4d896ba8b5bdb1ac1
Andrei <46579508+Andreisgl@users.noreply.github.com>
2024-10-10 09:26:28 -0300

    Basicão, mas tá bom por enquanto

--- operacoes.py
+++ operacoes.py
@@ -1,1 +1,13 @@
 '''Neste arquivo estão localizados os métodos de alteração de arquivo'''
+
+
+def main():
+    pass
+
+if __file__ == '__main__':
+    main()
+
+
+
+def somar_linhas(dados:bytes):
+    dados_string = dados

9a1100cf94cee09daabfc770afb64bee8459cef8
Andrei <46579508+Andreisgl@users.noreply.github.com>
2024-11-07 21:54:14 -0300

    squash

--- operacoes.py
+++ operacoes.py
@@ -1,18 +1,7 @@
 '''Neste arquivo estão localizados os métodos de alteração de arquivo'''
 
 
-#def main():
-#    pass
-#
-#if __file__ == '__main__':
-#    main()
-
-
 def resolver(dados:str):
     saida = 'testeeee'
 
     return saida
-
-
-def somar_linhas(dados:bytes):
-    dados_string = dados

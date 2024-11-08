6cf75bd234d71d630129e5fdf1686240625f8dda
Andrei <46579508+Andreisgl@users.noreply.github.com>
2024-11-07 22:23:43 -0300

    operações funcionam

--- operacoes.py
+++ operacoes.py
@@ -1,7 +1,42 @@
 '''Neste arquivo estão localizados os métodos de alteração de arquivo'''
 
+import numpy as np
+from asteval import Interpreter
+
 
 def resolver(dados:str):
     saida = 'testeeee'
 
+    print('ARQUIVO DE ENTRADA')
+    print(dados)
+    print('FIM/ARQUIVO DE ENTRADA')
+
+    linhas = dados.splitlines()
+    resultados = []
+
+    for linha in linhas:
+        resultados.append(avaliar_expressao(linha))
+
+
+    saida = ''
+
+    # Passar resultados para string
+    comp = len(resultados)
+    for i, r in enumerate(resultados):
+        saida += str(r)
+        if i < comp-1:
+            saida += '\n'
+
+
+
     return saida
+
+
+def avaliar_expressao(expression):
+    ae = Interpreter()
+    return ae(expression)
+
+    
+
+
+    
